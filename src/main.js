dd1f4b87757e7018e47381ef4f642e3bc5e8f42a
alex-kostenko <0984181760ss@gmail.com>
2019-08-07 17:52:27 +0300

    refactor sidebar

--- src/main.js
+++ src/main.js
@@ -6,3 +6,11 @@ import "./support.pug";
 import "./create_account.pug";
 import "./account_info.pug";
 import "./support_confirm.pug";
+
+(function () {
+  var sidebarElement = document.getElementById("sidebar");
+  document.getElementById("toogle-menu").onclick = function() { 
+    // sidebarElement
+    sidebarElement.classList.toggle('sidebar-compact');
+  }
+})();
