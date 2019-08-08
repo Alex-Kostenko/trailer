93769f58f3d447ac08d294d8055a17a099b88c02
alex-kostenko <0984181760ss@gmail.com>
2019-08-08 11:29:05 +0300

    fix sidebar behavior

--- src/main.js
+++ src/main.js
@@ -9,26 +9,8 @@ import "./support_confirm.pug";
 
 (function () {
   var sidebarElement = document.getElementById("sidebar");
-  var isMobile = undefined;
-
-  function updateWindowSize() {
-    var windowWidth = document.body.offsetWidth;
-    isMobile = windowWidth < 1000;
-    return isMobile;
-  }
-
-  function handleResize() {
-    if (isMobile !== updateWindowSize()) {
-      var action = isMobile ? 'add' : 'remove';
-      sidebarElement.classList[action]('sidebar_mobile');
-    }
-
-    return handleResize;
-  }
 
   document.getElementById("toogle-menu").addEventListener('click', function() {
     sidebarElement.classList.toggle('sidebar_open');
   });
-
-  window.addEventListener('resize', handleResize());
 })();
