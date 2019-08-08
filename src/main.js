f4ee88b49ba6626adef3b2697e3c88a007d337b1
alex-kostenko <0984181760ss@gmail.com>
2019-08-08 17:32:04 +0300

    add tabs

--- src/main.js
+++ src/main.js
@@ -10,10 +10,38 @@ import "./create_account.pug";
 import "./account_info.pug";
 import "./support_confirm.pug";
 
-(function () {
+(function() {
   var sidebarElement = document.getElementById("sidebar");
 
-  document.getElementById("toogle-menu").addEventListener('click', function() {
-    sidebarElement.classList.toggle('sidebar_open');
+  document.getElementById("toogle-menu").addEventListener("click", function() {
+    sidebarElement.classList.toggle("sidebar_open");
   });
+})();
+
+(function() {
+  var tablinks = document.getElementsByClassName("tab__links");
+  var tabcontents = document.getElementsByClassName("tab__content");
+
+  function closeAll() {
+    for (let index = 0; index < tabcontents.length; index++) {
+      tabcontents[index].style.display = "none";
+    }
+  }
+
+  function showSelected(id) {
+    document.getElementById(`${id}-container`).style.display = "block";
+  }
+
+  for (let index = 0; index < tablinks.length; index++) {
+    tablinks[index].addEventListener("click", function() {
+      closeAll();
+      showSelected(tablinks[index].id);
+
+      //TODO add active
+      // document.getElementById(tablinks[index]).className.remove(" active", "");
+      // document.getElementById(tablinks[index]).className += " active";
+    });
+  }
+
+  document.getElementById("Home").click();
 })();
