a361dd33df9fdda10eef66eb6a816cd917d84ca3
Alex <0984181760ss@gmail.com>
2019-08-11 12:13:53 +0300

    Feature/st trailers (#17)
    * add tabs and trailers home

--- src/main.js
+++ src/main.js
@@ -10,10 +10,42 @@ import "./create_account.pug";
 import "./account_info.pug";
 import "./support_confirm.pug";
 
-(function () {
+(function() {
   var sidebarElement = document.getElementById("sidebar");
 
-  document.getElementById("toogle-menu").addEventListener('click', function() {
-    sidebarElement.classList.toggle('sidebar_open');
+  document.getElementById("toogle-menu").addEventListener("click", function() {
+    sidebarElement.classList.toggle("sidebar_open");
   });
+})();
+
+(function() {
+  var tablinks = document.getElementsByClassName("tab__links");
+  var tabcontents = document.getElementsByClassName("tab__content");
+
+  function closeAll() {
+    for (let index = 0; index < tabcontents.length; index++) {
+      tabcontents[index].style.display = "none";
+    }
+  }
+
+  function removeClasses() {
+    for (let index = 0; index < tablinks.length; index++) {
+      tablinks[index].classList.remove("tab-active");
+    }
+  }
+
+  function showSelected(tablink) {
+    document.getElementById(`${tablink.id}-container`).style.display = "block";
+    removeClasses();
+    tablink.classList.add("tab-active");
+  }
+
+  for (let index = 0; index < tablinks.length; index++) {
+    tablinks[index].addEventListener("click", function() {
+      closeAll();
+      showSelected(tablinks[index]);
+    });
+  }
+
+  document.getElementById("Home").click();
 })();
