5f25716403bf83d70cea50e10f537eeab12b492d
Alex <0984181760ss@gmail.com>
2019-08-12 16:40:15 +0300

    trailer page adaptive (#20)

--- src/main.js
+++ src/main.js
@@ -19,6 +19,137 @@ import "./support_confirm.pug";
 })();
 
 (function() {
+  document.addEventListener("DOMContentLoaded", function(event) {
+    var totalLease = document.getElementById("TotalLease").getContext("2d");
+    var totalMaintenance = document.getElementById("TotalMaintenance").getContext("2d");
+    var totalTrailers = document.getElementById("TotalTrailers").getContext("2d");
+    
+    var chart = new Chart(totalLease, {
+      // The type of chart we want to create
+      type: "bar",
+      scales: {
+        xAxes: [{
+            categoryPercentage: 0.5,
+            barPercentage: 0.5,
+            barThickness: 2,
+            maxBarThickness: 4,
+            minBarLength: 2,
+            gridLines: {
+                offsetGridLines: true
+            }
+        }]
+    },
+
+      // The data for our dataset
+      data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: "My First dataset",
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            data: [0, 10, 5, 2, 20, 30, 45]
+          }
+        ]
+      },
+
+      // Configuration options go here
+      options: {}
+    });
+
+    var chart = new Chart(totalMaintenance, {
+      // The type of chart we want to create
+      type: "bar",
+      scales: {
+        xAxes: [{
+            categoryPercentage: 0.5,
+            barPercentage: 0.5,
+            barThickness: 2,
+            maxBarThickness: 4,
+            minBarLength: 2,
+            gridLines: {
+                offsetGridLines: true
+            }
+        }]
+    },
+
+      // The data for our dataset
+      data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: "My First dataset",
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            data: [0, 10, 5, 2, 20, 30, 45]
+          }
+        ]
+      },
+
+      // Configuration options go here
+      options: {}
+    });
+
+    var chart = new Chart(totalTrailers, {
+      // The type of chart we want to create
+      type: "bar",
+      scales: {
+        xAxes: [{
+            categoryPercentage: 0.5,
+            barPercentage: 0.5,
+            barThickness: 2,
+            maxBarThickness: 4,
+            minBarLength: 2,
+            gridLines: {
+                offsetGridLines: true
+            }
+        }]
+    },
+
+      // The data for our dataset
+      data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: "My First dataset",
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            data: [0, 10, 5, 2, 20, 30, 45]
+          }
+        ]
+      },
+
+      // Configuration options go here
+      options: {}
+    });
+  });
+})();
+
+(function() {
   var tablinks = document.getElementsByClassName("tab__links");
   var tabcontents = document.getElementsByClassName("tab__content");
 
